50fd4bda4aef7a464f957cb9450f1d8ca2e2e9d7
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-28 19:35:16 +0300

    Initial Commit

---
+++ PickerInsideMenu/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PickerInsideMenu
+//
+//  Created by Ramill Ibragimov on 28.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PickerInsideMenu/PickerInsideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerInsideMenuApp.swift
+//  PickerInsideMenu
+//
+//  Created by Ramill Ibragimov on 28.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct PickerInsideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
